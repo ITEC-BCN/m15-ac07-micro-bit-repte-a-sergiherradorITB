d0811dee87a87b29b23f71e4776da8c51c8f22be
sergiherradorITB <146918553+sergiherradorITB@users.noreply.github.com>
2024-10-24 17:45:20 +0200

    Opcion de jugar contra la maquina

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 input.onButtonPressed(Button.A, function () {
+    opcio_maquina = llista_opcions._pickRandom()
     basic.showLeds(`
-        . . . . .
         . # # # .
         . # # # .
         . # # # .
-        . . . . .
+        . # # # .
+        . # # # .
         `)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -12,14 +13,17 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
+        . . . . .
         . # # # .
         . # # # .
         . # # # .
-        . # # # .
-        . # # # .
+        . . . . .
         `)
 })
+let opcio_maquina = 0
+let llista_opcions: number[] = []
+llista_opcions = [1, 2, 3]
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+	
 })

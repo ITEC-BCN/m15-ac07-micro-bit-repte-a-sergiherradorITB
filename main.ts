55ceda87245142223c9bf9a417e94a62ab1310c7
sergiherradorITB <146918553+sergiherradorITB@users.noreply.github.com>
2024-10-28 15:02:34 +0100

    Funcion maquina

--- main.ts
+++ main.ts
@@ -1,1 +1,112 @@
-
+input.onButtonPressed(Button.A, function () {
+    opcio_maquina = llista_opcions._pickRandom()
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+    if (opcio_maquina == 3) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (opcio_maquina == 1) {
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            # . # . #
+            # . # . #
+            . # . # .
+            `)
+    }
+    if (opcio_maquina == 2) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    opcio_maquina = llista_opcions._pickRandom()
+    basic.showIcon(IconNames.Scissors)
+    if (opcio_maquina == 2) {
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            # . # . #
+            # . # . #
+            . # . # .
+            `)
+    }
+    if (opcio_maquina == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (opcio_maquina == 1) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    opcio_maquina = llista_opcions._pickRandom()
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    if (opcio_maquina == 3) {
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            # . # . #
+            # . # . #
+            . # . # .
+            `)
+    }
+    if (opcio_maquina == 1) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (opcio_maquina == 2) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+})
+let opcio_maquina = 0
+let llista_opcions: number[] = []
+llista_opcions = [1, 2, 3]
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
